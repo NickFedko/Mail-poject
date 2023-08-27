b7221ddd1323f0448d3daeb001ed57b47b3b7cf7
Mykyta <mykyta@users-MacBook-Pro-688.local>
2023-08-27 15:37:33 +0300

    change name

--- src/components/DeliveryInfo.jsx
+++ src/components/DeliveryInfo.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function DeliveryInfo({image, title, info}) {
     return (
         <div className="text-sm grid grid-cols-12"> 
-            <FontAwesomeIcon icon={image} className="w-6 h-6"/>
+            <FontAwesomeIcon icon={image} className="w-5 h-5"/>
             <div className="col-span-11">
                 <p className="text-gray-700">{title}</p>
                 <p className="text-gray-500">{info}</p>
